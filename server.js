926681f70bd96bef856d8ae0acfc5be46d59bc2b
alemjc <alemjc2204@gmail.com>
2016-02-24 21:23:48 -0500

    setting models for heroku db.

--- server.js
+++ server.js
@@ -18,82 +18,82 @@ app.engine("handlebars", expressHandlebars({
 
 app.set("view engine", "handlebars");
 
+app.use("/static", express.static("public"));
+
+var sequelize = new Sequelize(process.env.DATABASE_URL);
+
+var Places = sequelize.define("place", {
+     address:{
+       type:Sequelize.STRING,
+       allowNull:false
+     },
+
+     pictures:{
+       type:Sequelize.STRING
+     },
+
+     hours:{
+       type:Sequelize.STRING
+     },
+     category:{
+       type:Sequelize.STRING,
+       allowNull:false
+     }
+
+ });
+
+ var Users = sequelize.define("user",{
+   firstName:{
+     type:Sequelize.STRING,
+     allowNull:false
+   },
+   lastName:{
+     type:Sequelize.STRING,
+     allowNull:false
+   },
+
+   userName:{
+     type:Sequelize.STRING,
+     allowNull:false,
+     validate:{
+       len:[5,20]
+     }
+   },
+
+   password:{
+     type:Sequelize.STRING,
+     allowNull:false,
+     validate:{
+       len:[5,2000]
+     }
+   },
+
+   birthday:{
+     type:Sequelize.STRING
+   }
+ });
+
+ var Ratings = sequelize.define("rating",{
+   comment:{
+     type:Sequelize.STRING
+   }
+
+ });
+
+ Users.belongsToMany(Places,{through:Ratings});
+ Places.belongsToMany(Users,{through:Ratings});
+
+
+ sequelize.sync().then(function(){
+
+
+ }).catch(function(err){
+
+ });
+
+
 app.listen(PORT, function() {
   console.log("LISTENING ON %s", PORT);
 });
-
-app.use("/static", express.static("public"));
-
-// var sequelize = new Sequelize(process.env.DATABASE_URL);
-
-// var Places = sequelize.define("place", {
-//     address:{
-//       type:Sequelize.STRING,
-//       allowNull:false
-//     },
-
-//     pictures:{
-//       type:Sequelize.STRING
-//     },
-
-//     hours:{
-//       type:Sequelize.STRING
-//     },
-//     category:{
-//       type:Sequelize.STRING,
-//       allowNull:false
-//     }
-
-// });
-
-// var Users = sequelize.define("user",{
-//   firstName:{
-//     type:Sequelize.STRING,
-//     allowNull:false
-//   },
-//   lastName:{
-//     type:Sequelize.STRING,
-//     allowNull:false
-//   },
-
-//   userName:{
-//     type:Sequelize.STRING,
-//     allowNull:false,
-//     validate:{
-//       len:[5,20]
-//     }
-//   },
-
-//   password:{
-//     type:Sequelize.STRING,
-//     allowNull:false,
-//     validate:{
-//       len:[5,2000]
-//     }
-//   },
-
-//   birthday:{
-//     type:Sequelize.STRING
-//   }
-// });
-
-// var Ratings = sequelize.define("rating",{
-//   comment:{
-//     type:Sequelize.STRING
-//   }
-
-// });
-
-// Users.belongsToMany(Places,{through:Ratings});
-// Places.belongsToMany(Users,{through:Ratings});
-
-
-// sequelize.sync().then(function(){
-
-
-// }).catch(function(err){
-
-// });
-
 
 
