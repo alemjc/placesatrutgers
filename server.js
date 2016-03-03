872a5e6ab0a5914c3551aef983848dd5316e233c
alemjc <alemjc2204@gmail.com>
2016-03-02 20:41:54 -0500

    added logout route.

--- server.js
+++ server.js
@@ -300,6 +300,13 @@ app.get("/login", function(req, res) {
   res.render("login");
 });
 
+app.get("/logout", function(req, res){
+  req.session.destroy();
+  req.logout();
+  res.redirect("/");
+
+});
+
 app.post("/login", passport.authenticate('local',{
     successRedirect:"/",
     failureRedirect:"/login"
